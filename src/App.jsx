879df5f67dcd66172a2c178bc8ffc3b6dea921e1
kiq-caique <caique-kiq@hotmail.com>
2023-05-11 15:49:44 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import Todo from './components/Todo'
+import "./App.css"
+
+function App() { 
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Primeira tarefa",
+      category: "Pessoal",
+      isComplete: false,
+    },
+    {
+      id: 2,
+      text: "Segunda tarefa",
+      category: "Profissional",
+      isComplete: false,
+    },
+    {
+      id: 3,
+      text: "Terceira tarefa",
+      category: "Estudos",
+      isComplete: false,
+    }
+  ])
+
+  return (
+      <div className='app'>
+        <h1>Hello World</h1>
+        <div className="todo-list">
+          {todos.map((todo) => (
+            <Todo todo={todo}/>
+          ))}
+        </div>
+      </div>
+      
+  )
+}
+
+export default App
